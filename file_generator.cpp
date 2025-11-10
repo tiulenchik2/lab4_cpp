0cbcb9c5ff360c380e75f8923f71d637674ec647
tiulenchik2 <moskaliuk.illia@knu.ua>
2025-11-11 01:37:12 +0200

    File generator, .gitgnore, testing threads in main

---
+++ file_generator.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <random>
+#include <format>
+
+void generate_file(const std::string& filename,
+const std::vector<double>& probs,
+const std::vector<std::string>& actions,
+long long total_lines){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::discrete_distribution<> dist(probs.begin(), probs.end());
+
+    std::ofstream outfile(filename);
+    if (!outfile){
+        std::cerr << "Error opening file " << filename << "\n";
+        return;
+    }
+    for (long long i = 0; i < total_lines; ++i)
+    {
+        int act_index = dist(gen);
+        outfile << actions[act_index]<<"\n";
+    }
+    std::cout << std::format("Finished {} ({} lines.)", filename, total_lines)<<"\n";
+}
+
+int main(){
+    const long long NUM_LINES = 1000000;
+    const std::vector<std::string> all_actions = {
+        "read 0",
+        "write 0 1",
+        "read 1",
+        "write 1 1",
+        "string"
+    };
+
+    std::vector<double> probs_A = {10.0, 5.0, 50.0, 10.0, 25.0};
+    generate_file("A.txt", probs_A, all_actions, NUM_LINES);
+    
+    std::vector<double> probs_B = {20.0, 20.0, 20.0, 20.0, 20.0};
+    generate_file("B.txt", probs_B, all_actions, NUM_LINES);
+
+    std::vector<double> probs_C = {5.0, 10.0, 5.0, 80.0, 0.0};
+    generate_file("C.txt", probs_C, all_actions, NUM_LINES);
+
+    std::cout<<"All files generated."<<"\n";
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
+#include <vector>
+#include <string>
+#include <format>
+#include <thread>
 #include <iostream>
+#include <mutex>
+class MyStruct{
+public:
+    MyStruct(int m) : m_fields(m){}
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    void set_field(int index, int value){
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_fields[index] = value;
+    }
+    int get_field(int index) const{
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_fields[index];
+    }
+    operator std::string() const{
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return std::format("Field 0: {}, Field 1: {}", m_fields[0], m_fields[1]);
+    }
+    void increment_field(int index){
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_fields[index]++;
+    }
+private:
+    std::vector<int> m_fields;
+    mutable std::mutex m_mutex;
+};
+
+void worker_func(MyStruct* structure){
+    for (int i = 0; i < 1000000; ++i){
+        structure->increment_field(0);
+    }
+    
+}
+
+int main(){
+    MyStruct mystruct(2);
+    std::cout<<"Init state: "<<std::string(mystruct)<<"\n";
+    std::cout<<"Start 2 threads..."<<"\n";
+    
+    std::thread t1(worker_func, &mystruct);
+    std::thread t2(worker_func, &mystruct);
+    t1.join();
+    t2.join();
+    
+    std::cout<<"Expected 2000000."<<"\n";
+    std::cout<<"Grantedd "<<mystruct.get_field(0)<<".\n";
     return 0;
 }
